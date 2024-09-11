8e25001cb01f57cabd09186f9c72cb4b845726e4
javiza <jonathan@gmail.com>
2024-09-10 23:30:02 -0300

    se establece conexion con SQLite, base de datos simple. creacion de servicio, interface, y componente. se agrega funciones asincronas

--- src/app/component/formulario/formulario.component.ts
+++ src/app/component/formulario/formulario.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms'
+import { FormControl, FormGroup, ReactiveFormsModule, FormsModule} from '@angular/forms'
 import { IonButton, IonItem, IonContent, IonLabel, IonInput, IonList} from '@ionic/angular/standalone'
+import { Prenda, PrendaService } from 'src/app/servicios/prenda.service';
 
 @Component({
   selector: 'app-formulario',
@@ -8,17 +9,48 @@ import { IonButton, IonItem, IonContent, IonLabel, IonInput, IonList} from '@ion
   styleUrls: ['./formulario.component.scss'],
   standalone: true,
   imports: [IonButton, IonItem, IonContent, IonLabel, IonInput, IonList,
-    ReactiveFormsModule
+    ReactiveFormsModule, FormsModule,
   ]
 })
-export class FormularioComponent  implements OnInit {
+export class FormularioComponent  {
+  prendas: Prenda[] = []
+  nombre: string = "";
+  cantidad!: number; 
+  descripcion: string = "";
 
+  constructor(
+    private dbService:PrendaService
+  ) {}
   
-  ngOnInit() {}
-  ropaForm = new FormGroup({
-      nombre: new FormControl('sabana'),
-      cantidad: new FormControl(2),
-      descripcion: new FormControl('descripcion')
-    })
+  
+  async ngOnInit() {
+    await this.dbService.IniciarPlugin();
+    this.actualizar()
+  }
+  async ngOnDestroy(){
+    await this.dbService.cerrarConexion();
+  }
+  async actualizar(){
+    this.prendas = await this.dbService.findAllPrendas()
+  }
+  async agregar() {
+    const prenda:Prenda = {nombre: this.nombre, cantidad: this.cantidad, descripcion: this.descripcion}
+    await this.dbService.addPrenda(prenda)
+    await this.actualizar()
+  }
+  async actualizarPrenda(prenda: Prenda){
+    const prendaEditada = {
+      id: prenda.id,
+      nombre: prenda.nombre,
+      cantidad: prenda.cantidad,
+      descripcion: prenda.descripcion
+    }
+  }
+  async eliminar(prenda: Prenda){
+    if(prenda.id){
+      await this.dbService.deletePrenda(prenda.id)
+      await this.actualizar()
+    }
+  }
 
 }
--- src/app/servicios/prenda.service.ts
+++ src/app/servicios/prenda.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
+import { CapacitorSQLite, SQLiteConnection, SQLiteDBConnection } from '@capacitor-community/sqlite'
 
-export interface Prendas {
+export interface Prenda {
+  id?: number
   nombre: string
   cantidad: number
   descripcion: string
@@ -10,23 +12,46 @@ export interface Prendas {
   providedIn: 'root'
 })
 export class PrendaService {
-  private prendas:Prendas[] = []
-  constructor() { 
-    this.prendas.push( {nombre: "campo verde grande", cantidad: 19, descripcion: "150cmx100cm"})
-    this.prendas.push( {nombre: "MCM", cantidad: 11, descripcion: "190x150"})
-    this.prendas.push( {nombre: "ranger", cantidad: 7, descripcion: "100cmx10cm"})
-  } 
-  add(prenda:Prendas){
-    this.prendas.push(prenda)
+  private sqlite: SQLiteConnection = new SQLiteConnection(CapacitorSQLite)
+  private db!: SQLiteDBConnection
+
+  constructor() { } 
+
+  async IniciarPlugin() {
+    this.db = await this.sqlite.createConnection(
+      "prendas.sqlite", false, 'no-encryption', 1, false
+    )
+    await this.db.open()
+    const schema = `
+      CREATE TABLE IF NOT EXISTS prendas(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      nombre TEXT NOT NULL,
+      cantidad INTEGER DEFAULT 0,
+      descripcion TEXT
+      );
+      `
+      await this.db.execute(schema)
   }
-  getAll():Prendas[]{
-    return this.prendas
+  async cerrarConexion() {
+    this.db.close()
   }
-  findIndex(prenda:Prendas){
-    return this.prendas.findIndex(p => p.nombre == prenda.nombre)
+  async addPrenda(prenda:Prenda){
+    const sql = `INSERT INTO prendas(nombre, cantidad, descripcion) VALUES(?,?)`
+    await this.db.run(sql, [prenda.nombre, prenda.cantidad, prenda.descripcion])
   }
-  delete(prenda:Prendas){
-    const indice = this.findIndex(prenda)
-    this.prendas.splice(indice,1)
+  async updatePrenda(prenda:Prenda){
+    const sql = `UPDATE prendas SET  nombre, cantidad, description = ? WHERE id = ?`
+    console.log("updatePrenda()")
+    console.log(`update prendas set nombre = ${prenda.nombre}, cantidad = ${prenda.cantidad}, decripcion = ${prenda.descripcion}`)
+    await this.db.run(sql, [prenda.nombre, prenda.cantidad, prenda.descripcion, prenda.id])
   }
+  async deletePrenda(id:number) {
+    const sql = `DELETE FROM prendas WHERE id = ?`
+    await this.db.run(sql, [id])
+  }
+  async findAllPrendas(){
+    const prendas = await this.db.query('SELECT * FROM prendas');
+    return prendas.values ?? []
+  }
+
 }
